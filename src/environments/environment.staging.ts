9d08a36b432d4d991131a6ef06e5e9d3e7e5f9ba
duycs <duycs@outlook.com>
2023-01-05 16:03:29 +0700

    update timeout

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@ export const environment = {
     ssoUrl: 'https://sso-identity-server.herokuapp.com',
     apiUrl: 'https://weedit-api.herokuapp.com',
     appUrl: 'http://weedit-app.s3-website-ap-northeast-1.amazonaws.com',
-    loadTimeout: 4000,
+    loadTimeout: 8000,
 };
